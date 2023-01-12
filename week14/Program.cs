267d026c0e79a1f6cee913051e4a3f9c845795c5
kulla <kujakobs@tktk.ee>
2023-01-12 15:17:57 +0200

    add week2 code

---
+++ week14/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("name your dog");
+string myDogName = Console.ReadLine();
+
+Dog mydog = new Dog(myDogName);
+Dog neigouboursdog = new Dog("good girl");
+Console.WriteLine($"my dog name is {mydog.Name}");
+Console.WriteLine($"my neighbours dog name is {neigouboursdog.Name}");
+
+
+mydog.Rename("Bad Boy");
+while (mydog.LevelOfHapiness != 5)
+{
+    mydog.Bark();
+}
+
+mydog.WagTail();
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has beed renamed to: {newName}");
+    }
+    public void Bark()
+    {
+        Console.WriteLine("woof-woof");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("wogglr-wiggle");
+    }
+}
+
